d98cdaf49b4f6ea55934fc475375ba6acb291411
strongmhk <kim6562166086@gmail.com>
2023-07-18 14:17:41 +0900

    이미지 서비스 로직 구성

---
+++ photogram/src/main/java/com/cos/photogramstart/domain/image/Image.java
@@ -0,0 +1,40 @@
+package com.cos.photogramstart.domain.image;
+
+import com.cos.photogramstart.domain.user.User;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity // 디비에 테이블을 생성
+public class Image { // 이미지 -> 유저 = 1 : 1, 유저 -> 이미지 = 1 : N
+    @Id // primary key로 지정
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // 번호 증가 전략이 데이터베이스를 따라간다.
+    private int id;
+    private String caption; // 사진 설명
+    private String postImageUrl; // 사진을 전송받아서 그 사진을 서버에 특정 폴더에 저장 - DB에 그 저장된 경로를 insert(
+
+    @JoinColumn(name = "userId") // DB에 객체를 저장하면 FK로 저장되기 때문에 FK의 이름 정해줌
+    @ManyToOne
+    private User user; // 누가 업로드 했는지
+
+    // 이미지 좋아요
+
+    // 댓글
+
+    private LocalDateTime createDate; // 데이터가 언제들어왔는지
+
+    @PrePersist // 디비에 insert 되기 직전에 실행
+    public void createDate(){
+        this.createDate = LocalDateTime.now();
+    }
+
+}
+
---
+++ photogram/src/main/java/com/cos/photogramstart/service/ImageService.java
@@ -0,0 +1,46 @@
+package com.cos.photogramstart.service;
+
+import com.cos.photogramstart.config.auth.PrincipalDetails;
+import com.cos.photogramstart.domain.image.Image;
+import com.cos.photogramstart.domain.image.ImageRepository;
+import com.cos.photogramstart.web.dto.image.ImageUploadDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class ImageService {
+
+    private final ImageRepository imageRepository;
+
+    @Value("${file.path}")
+    private String uploadFolder;
+
+    public void 사진업로드(ImageUploadDto imageUploadDto, PrincipalDetails principalDetails){
+        UUID uuid = UUID.randomUUID(); // 중복이 안되는 고유성을 가진 숫자 생성
+        String imageFileName = uuid + "_" + imageUploadDto.getFile().getOriginalFilename(); // + 1.jpg
+        System.out.println("imageFileName = " + imageFileName);
+
+        Path imageFilePath = Paths.get(uploadFolder + imageFileName); // C:/project/Springboot/upload/1.jpg
+
+        // 통신, 디스크 I/O -> 예외가 발생할 수 있다.(실제 디스크에는 해당 파일명을 가진 파일이 없다던지)
+        try{
+            Files.write(imageFilePath, imageUploadDto.getFile().getBytes());
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+
+        // image 테이블에 저장
+        Image image = imageUploadDto.toEntity(imageFileName, principalDetails.getUser()); //
+        Image imageEntity = imageRepository.save(image);
+
+        System.out.println(imageEntity);
+    }
+
+}
