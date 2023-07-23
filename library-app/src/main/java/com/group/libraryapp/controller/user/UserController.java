a8afd41db5539dbf85ffd82fa3e511551c68e32e
strongmhk <kim6562166086@gmail.com>
2023-07-23 15:21:29 +0900

    의존관계 주입 예제 - 인터페이스(FruitService) 구현체(AppleService, BananaService, OrangeService

--- library-app/src/main/java/com/group/libraryapp/controller/user/UserController.java
+++ library-app/src/main/java/com/group/libraryapp/controller/user/UserController.java
@@ -1,10 +1,12 @@
-package com.group.libraryapp.user;
+package com.group.libraryapp.controller.user;
 
 import com.group.libraryapp.domain.User;
 import com.group.libraryapp.dto.user.request.UserCreateRequest;
 import com.group.libraryapp.dto.user.request.UserUpdateRequest;
 import com.group.libraryapp.dto.user.response.UserResponse;
-import com.group.libraryapp.service.UserService;
+import com.group.libraryapp.service.fruit.FruitService;
+import com.group.libraryapp.service.user.UserService;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -14,10 +16,11 @@ import java.util.List;
 public class UserController {
 
     private final UserService userService;
+    private final FruitService fruitService;
 
-
-    public UserController(UserService userService){
+    public UserController(UserService userService, @Qualifier("main") FruitService fruitService){
         this.userService = userService;
+        this.fruitService = fruitService;
     }
 
     private final List<User> users = new ArrayList<>();
--- library-app/src/main/java/com/group/libraryapp/service/user/UserService.java
+++ library-app/src/main/java/com/group/libraryapp/service/user/UserService.java
@@ -1,4 +1,4 @@
-package com.group.libraryapp.service;
+package com.group.libraryapp.service.user;
 
 import com.group.libraryapp.dto.user.request.UserCreateRequest;
 import com.group.libraryapp.dto.user.request.UserUpdateRequest;
