df3d60b98a19ea9c12d642898a13076826eca996
strongmhk <kim6562166086@gmail.com>
2023-07-23 10:59:32 +0900

    Controller, Service, Repository 3단 분리 후 각각 스프링 빈으로 등록

---
+++ library-app/src/main/java/com/group/libraryapp/service/UserService.java
@@ -0,0 +1,47 @@
+package com.group.libraryapp.service;
+
+import com.group.libraryapp.dto.user.request.UserCreateRequest;
+import com.group.libraryapp.dto.user.request.UserUpdateRequest;
+import com.group.libraryapp.dto.user.response.UserResponse;
+import com.group.libraryapp.repository.user.UserRepository;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public void saveUser(UserCreateRequest request) {
+        userRepository.saveUser(request.getName(), request.getAge());
+    }
+
+    public List<UserResponse> getUsers() {
+        return userRepository.getUsers();
+    }
+
+    public void updateUser(UserUpdateRequest request) {
+        if (userRepository.isUserNotExist(request.getId())) {
+            throw new IllegalArgumentException();
+        }
+
+        userRepository.updateUserName(request.getName(), request.getId());
+
+    }
+
+    public void deleteUser(String name) {
+        if (userRepository.isUserNotExist(name)) {
+            throw new IllegalArgumentException();
+        }
+
+        userRepository.deleteUser(name);
+    }
+
+
+}
