bf03fabc36fd80c1e4554397a38dc509c71e1c6f
strongmhk <kim6562166086@gmail.com>
2023-07-24 12:02:34 +0900

    서비스 구현체 분리

--- library-app/src/main/java/com/group/libraryapp/service/user/UserService.java
+++ library-app/src/main/java/com/group/libraryapp/service/user/UserService.java
@@ -3,44 +3,18 @@ package com.group.libraryapp.service.user;
 import com.group.libraryapp.dto.user.request.UserCreateRequest;
 import com.group.libraryapp.dto.user.request.UserUpdateRequest;
 import com.group.libraryapp.dto.user.response.UserResponse;
-import com.group.libraryapp.repository.user.UserRepository;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-@Service
-public class UserService {
+public interface UserService {
 
-    private final UserRepository userRepository;
+    void saveUser(UserCreateRequest request);
 
-    public UserService(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
+    List<UserResponse> getUsers();
 
-    public void saveUser(UserCreateRequest request) {
-        userRepository.saveUser(request.getName(), request.getAge());
-    }
+    void updateUser(UserUpdateRequest request);
 
-    public List<UserResponse> getUsers() {
-        return userRepository.getUsers();
-    }
-
-    public void updateUser(UserUpdateRequest request) {
-        if (userRepository.isUserNotExist(request.getId())) {
-            throw new IllegalArgumentException();
-        }
-
-        userRepository.updateUserName(request.getName(), request.getId());
-
-    }
-
-    public void deleteUser(String name) {
-        if (userRepository.isUserNotExist(name)) {
-            throw new IllegalArgumentException();
-        }
-
-        userRepository.deleteUser(name);
-    }
+    void deleteUser(String name);
 
 
 }
