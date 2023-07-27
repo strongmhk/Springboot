29eef99e2306553e6c1db3b14d4a9389a542a8eb
strongmhk <kim6562166086@gmail.com>
2023-07-27 14:37:06 +0900

    좋아요 모델, api 구현

---
+++ photogram/src/main/java/com/cos/photogramstart/domain/likes/Likes.java
@@ -0,0 +1,48 @@
+package com.cos.photogramstart.domain.likes;
+
+import com.cos.photogramstart.domain.image.Image;
+import com.cos.photogramstart.domain.user.User;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity // 디비에 테이블을 생성
+@Table(
+        uniqueConstraints = {
+                @UniqueConstraint(
+                        name = "likes_uk",
+                        columnNames = {"imageId", "userId"} // 실제 db의 컬럼 명을 적기
+                )
+        }
+)
+public class Likes { // N
+
+    @Id // primary key로 지정
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // 번호 증가 전략이 데이터베이스를 따라간다.
+    private int id;
+
+    @JoinColumn(name = "imageId")
+    @ManyToOne
+    private Image image; // 1
+
+    // 오류가 터지고 나서 잡아보자
+    @JoinColumn(name = "userId")
+    @ManyToOne
+    private User user; // 1
+
+    private LocalDateTime createDate; // 데이터가 언제들어왔는지
+
+    @PrePersist // 디비에 insert 되기 직전에 실행
+    public void createDate(){
+        this.createDate = LocalDateTime.now();
+    }
+
+}
