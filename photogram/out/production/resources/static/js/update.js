b78263ebb7279f9b9fbcd8d4090abff9a5d77a24
strongmhk <kim6562166086@gmail.com>
2023-07-31 14:51:26 +0900

    뭐지 이거

---
+++ photogram/out/production/resources/static/js/update.js
@@ -0,0 +1,26 @@
+// (1) 회원정보 수정
+function update(userId, event) {
+    event.preventDefault(); // 폼 태그 액션을 막기!
+    let data = $("#profileUpdate").serialize(); // key = value
+
+    console.log(data);
+
+    $.ajax({
+        type: "put",
+        url: `/api/user/${userId}`,
+        data: data,
+        contentType: "application/x-www-form-urlencoded; charset=utf-8",
+        dataType: "json"
+    }).done(res=>{ // HttpStatus 상태코드 200번대
+        console.log("update 성공", res);
+        alert("회원정보가 성공적으로 수정되었습니다.");
+        location.href=`/user/${userId}`;
+    }).fail(error=>{ // HttpStatus 상태코드 200번대가 아닐 때
+        if(error.data == null){
+            alert(error.responseJSON.message);
+        }else{
+            alert(JSON.stringify(error.responseJSON.data));
+        }
+
+    });
+}
---
+++ photogram/src/main/java/com/cos/photogramstart/web/dto/comment/CommentDto.java
@@ -0,0 +1,16 @@
+package com.cos.photogramstart.web.dto.comment;
+
+import lombok.Data;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@Data
+public class CommentDto {
+
+    @NotBlank // 빈값이거나 null 체크, 빈 공백까지
+    private String content;
+
+    @NotNull // null 체크
+    private Integer imageId;
+}
