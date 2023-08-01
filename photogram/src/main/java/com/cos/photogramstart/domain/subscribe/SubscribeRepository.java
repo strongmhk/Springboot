f28335f1ace3413e410fd0e418e6c23ca32733f2
strongmhk <kim6562166086@gmail.com>
2023-08-02 03:51:14 +0900

    구독 문제 해결 -> 네이티브 쿼리에 @Param 붙여주기

--- photogram/src/main/java/com/cos/photogramstart/domain/subscribe/SubscribeRepository.java
+++ photogram/src/main/java/com/cos/photogramstart/domain/subscribe/SubscribeRepository.java
@@ -10,11 +10,11 @@ public interface SubscribeRepository extends JpaRepository<Subscribe, Integer> {
     // 메서드 이름 맨 앞의 m은 내가 직접만들었다는 약어
     @Modifying // INSERT, DELETE, UPDATE를 네이티브 쿼리로 작성하려면 해당 어노테이션 필요
     @Query(value = "INSERT INTO subscribe(fromUserId, toUserId, createDate) VALUES(:fromUserId, :toUserId,now())",nativeQuery = true) // :fromUserId, 파라미터로 받은 fromUserId 값을 넣어준다는 뜻
-    void mSubscribe(int fromUserId, int toUserId);
+    void mSubscribe(@Param("fromUserId")int fromUserId, @Param("toUserId")int toUserId);
 
     @Modifying
     @Query(value = "DELETE FROM subscribe WHERE fromUserId = :fromUserId AND toUserId = :toUserId",nativeQuery = true)
-    void mUnSubscribe(int fromUserId, int toUserId);
+    void mUnSubscribe(@Param("fromUserId")int fromUserId, @Param("toUserId")int toUserId);
 
     @Query(value = "SELECT COUNT(*) FROM subscribe WHERE fromUserId = :principalId AND toUserId = :pageUserId", nativeQuery = true)
     int mSubscribeState(@Param("principalId")int principalId, @Param("pageUserId")int pageUserId);
